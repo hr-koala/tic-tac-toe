cb56b3010dbc6d88d2deb011bca4ff7efb5c40e7
hr-koala <40801893+hr-koala@users.noreply.github.com>
2019-06-19 17:14:46 +0800

    Add files via upload

---
+++ mydemo/build/precache-manifest.d160acf7b8d6fdad4b81853cf3d95fa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b519c70b9c26ec213fedf5ca370c293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c22ab8b362c398bb8af7",
+    "url": "/static/css/main.42229c55.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "c22ab8b362c398bb8af7",
+    "url": "/static/js/main.c8c7a7de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
